dd10f00518e9a702561447a36b7d1aadff5fdef6
tunip3 <ts-gaming@outlook.com>
2018-09-09 17:39:50 +0100

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <stdio.h>
+#include <intrin.h>
+#include <string>
+#include <tchar.h>
+#include <Windows.h>
+#include <vector>
+#include <psapi.h>
+#include <SubAuth.h>
+#include <iostream>
+
+
+
+int main(int argc, char *argv[])
+{
+	system("mkdir D:\\DevelopmentFiles\\links");
+	system("mklink /J D:\\DevelopmentFiles\\links\\C C:\\");
+	system("mklink /J D:\\DevelopmentFiles\\links\\D D:\\");
+	system("mklink /J D:\\DevelopmentFiles\\links\\E E:\\");
+	system("mklink /J D:\\DevelopmentFiles\\links\\J J:\\");
+	system("mklink /J D:\\DevelopmentFiles\\links\\M M:\\");
+	system("mklink /J D:\\DevelopmentFiles\\links\\N N:\\");
+	system("mklink /J D:\\DevelopmentFiles\\links\\O O:\\");
+	system("mklink /J D:\\DevelopmentFiles\\links\\P P:\\");
+	system("mklink /J D:\\DevelopmentFiles\\links\\Q Q:\\");
+	system("mklink /J D:\\DevelopmentFiles\\links\\S S:\\");
+	system("mklink /J D:\\DevelopmentFiles\\links\\T T:\\");
+	system("mklink /J D:\\DevelopmentFiles\\links\\U U:\\");
+	system("mklink /J D:\\DevelopmentFiles\\links\\W W:\\");
+	system("mklink /J D:\\DevelopmentFiles\\links\\X X:\\");
+	system("mklink /J D:\\DevelopmentFiles\\links\\Y Y:\\");
+	system("mklink /J D:\\DevelopmentFiles\\links\\volume1 \\\\?\\GLOBALROOT\\Device\\HarddiskVolume{0}\\");
+}
